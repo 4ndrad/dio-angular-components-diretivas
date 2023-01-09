298f4346517752fe8fbead3467b1945e8df86a0b
Natan Andrade de Lima <natanal@primeit.com.br>
2023-01-09 14:56:19 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ListaModule } from './lista/lista.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CompAtributosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ListaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
